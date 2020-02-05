24a92bc100faea9cf33d8896fbf75e598a40d16c
sumancrest0001 <suman.crest0001@gmail.com>
2020-02-06 04:53:59 +0545

    frontend part completed

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+module.exports = {
+  entry: ["babel-polyfill", "./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  stats: { children: false },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
